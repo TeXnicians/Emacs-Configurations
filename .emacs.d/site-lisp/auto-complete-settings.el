5d0aea888969ef3cf6b71d49ea780ad97b6f5b3b
Zhaodong Wang <wd20060220@gmail.com>
2014-09-17 21:07:39 -0500

    Auto Complete

--- .emacs.d/site-lisp/auto-complete-settings.el
+++ .emacs.d/site-lisp/auto-complete-settings.el
@@ -10,8 +10,38 @@
 (setq ac-use-fuzzy t
       turn-on-fuzzy-isearch t)
 (ac-set-trigger-key "RET")
-(setq ac-expand-on-auto-complete 0)
+; (setq ac-expand-on-auto-complete 0)
 
-(require 'auto-complete-latex)
+(require 'pos-tip)
+; (setq ac-quick-help-prefer-pos-tip t)   ;default is t
+; (setq ac-use-quick-help t)
+; (setq ac-quick-help-delay 1.0)
+
+; (require 'auto-complete-latex)
+
+(add-hook 'LaTeX-mode-hook
+    (lambda ()
+    (setq ac-quick-help-prefer-pos-tip t)   ;default is t
+    (setq ac-use-quick-help t)
+    (setq ac-quick-help-delay 1.0)
+    (auto-complete-mode t)
+    (setq ac-sources (append
+      '(ac-source-dictionary
+          ac-source-abbrev
+          ac-source-words-in-buffer
+          ac-source-words-in-same-mode-buffers
+          ac-source-files-in-current-dir
+          ac-source-filename) ac-sources))
+    ; ; (setq ac-sources
+    ;     '(ac-source-dictionary
+    ;       ac-source-semantic
+    ;       ac-source-yasnippet
+    ;       ac-source-abbrev
+    ;       ac-source-words-in-buffer
+    ;       ac-source-words-in-same-mode-buffers
+    ;       ac-source-files-in-current-dir
+    ;       ac-source-filename))
+))
+
 
 (provide 'auto-complete-settings)
